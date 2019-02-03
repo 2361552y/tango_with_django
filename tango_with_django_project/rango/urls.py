bd8fb7d4d65ea8d7c083a064217625fbc79848a5
YIBO YANG <46564270+2361552y@users.noreply.github.com>
2019-02-03 22:35:31 +0000

    Add files via upload

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from rango import views
 
+# app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
@@ -10,3 +11,4 @@ urlpatterns = [
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
 ]
+
